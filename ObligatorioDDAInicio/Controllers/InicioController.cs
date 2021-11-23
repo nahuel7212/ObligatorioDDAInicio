1e57253366847701ce288d0e359f4fb6f76be721
nahuel7212 <nahuelpereira7212@gmail.com>
2021-11-23 18:37:09 -0300

    first commit

---
+++ ObligatorioDDAInicio/Controllers/InicioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ObligatorioDDAInicio.Controllers
+{
+    public class InicioController : Controller
+    {
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+    }
+}
